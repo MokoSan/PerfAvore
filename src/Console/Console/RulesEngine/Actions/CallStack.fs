073c75ccf037ab1fc1d35cf7fd3b1dd0a855ec37
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2021-12-18 00:42:14 -0800

    Added a tree view for the call stack

--- src/Console/Console/RulesEngine/Actions/CallStack.fs
+++ src/Console/Console/RulesEngine/Actions/CallStack.fs
@@ -7,24 +7,26 @@ open Microsoft.Diagnostics.Tracing.Etlx
 open Microsoft.Diagnostics.Symbols
 
 open RulesEngine.Domain
+open Spectre.Console
 
 let symbolReader : SymbolReader = new SymbolReader(TextWriter.Null, SymbolPath.SymbolPathFromEnvironment)
 
 // Helper fn responsible for getting the call stack from a particular trace event.
-let printCallStack (rule: Rule) (traceEvent : TraceEvent) : unit =
+let printCallStack (rule: RulesEngine.Domain.Rule) (traceEvent : TraceEvent) : unit =
 
     let callStack = traceEvent.CallStack()
     if isNull callStack then 
         printfn $"Rule: {rule.InputRule} invoked for Event: {traceEvent} however, the call stack associated with the event is null." 
         ()
 
-    printfn $"Rule: {rule.InputRule} invoked for Event: {traceEvent} with CallStack: \n"
+    let root = Tree($"{rule.InputRule.EscapeMarkup()}");
 
     let printStackFrame (callStack : TraceCallStack) : unit =
         if not (isNull callStack.CodeAddress.ModuleFile)
         then
             callStack.CodeAddress.CodeAddresses.LookupSymbolsForModule(symbolReader, callStack.CodeAddress.ModuleFile)
-            printfn "%s!%s" callStack.CodeAddress.ModuleName callStack.CodeAddress.FullMethodName
+            let frameValue = sprintf "%s!%s" callStack.CodeAddress.ModuleName callStack.CodeAddress.FullMethodName
+            root.AddNode ( frameValue.EscapeMarkup() ) |> ignore
 
     let rec processFrame (callStack : TraceCallStack) : unit =
         if isNull callStack then ()
@@ -33,4 +35,5 @@ let printCallStack (rule: Rule) (traceEvent : TraceEvent) : unit =
             processFrame callStack.Caller
     
     processFrame callStack
+    AnsiConsole.Write root
     printfn "\n"
