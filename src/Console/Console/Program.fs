d51e54a4809f67fbdf6a7dd569b02f5a5a4ed92f
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2021-12-17 17:06:53 -0800

    Added logic for realtime sessions

--- src/Console/Console/Program.fs
+++ src/Console/Console/Program.fs
@@ -10,36 +10,27 @@ open RulesEngine.Domain
 open RulesEngine.ActionEngine
 open Microsoft.Diagnostics.Tracing
 open Microsoft.Diagnostics.Tracing.Session
+open Microsoft.Diagnostics.Tracing.Parsers;
 
 [<EntryPoint>]
 let main argv =
     AnsiConsole.MarkupLine("[underline green]Rule Based Performance Analysis: MokoSan's 2021 F# Advent Submission![/] ");
 
     let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
-
     let parser      = ArgumentParser.Create<Arguments>(errorHandler = errorHandler)
     let parsedArgs  = parser.Parse(inputs = argv, raiseOnUsage = true)
 
     // Get Command Line Args 
-    let tracePathArgs = parsedArgs.GetResult TracePath
     let processName   = parsedArgs.GetResult ProcessName
+    let tracePathArgs = parsedArgs.GetResult TracePath
 
-    let rules = 
+    let parsedRules =
         [ "GC/AllocationTick.AllocationAmount > 108000: Print CallStack"; 
           "GC/AllocationTick.AllocationAmount > 200000: Print CallStack"; 
           "ThreadPoolWorkerThreadAdjustment/Stats.Throughput < 4: Print CallStack"; ]
-
-    let parsedRules = 
-        rules
         |> List.map(parseRule)
 
     let traceLog = getTraceLogFromTracePath tracePathArgs
-
-    use traceEventSession = new TraceEventSession($"GCRealMonSession_{Guid.NewGuid()}");
-    let action = Action<TraceEvent>(fun e -> printfn "%A" e)
-    traceEventSession.Source.add_AllEvents (action)
-    let thisHasSideEffects = traceEventSession.Source.Process() 
-
 
     let events = traceLog.Events 
     let applyRule (events : TraceEvent seq) (rule : Rule) = 
--- src/Console/Console/TraceSession.fs
+++ src/Console/Console/TraceSession.fs
@@ -3,6 +3,15 @@ module TraceSession
 open System
 open Microsoft.Diagnostics.Tracing.Etlx
 open Microsoft.Diagnostics.Tracing.Session
+open Microsoft.Diagnostics.Tracing.Parsers
+open Microsoft.Diagnostics.Tracing
 
 let getTraceLogFromTracePath (tracePath : string) : TraceLog = 
     TraceLog.OpenOrConvert tracePath
+
+let getRealTimeSession : TraceEventSession = 
+    let traceEventSession : TraceEventSession = new TraceEventSession($"Session_{Guid.NewGuid()}");
+    let keywords : uint64 = uint64(ClrTraceEventParser.Keywords.GC ||| ClrTraceEventParser.Keywords.Threading ||| ClrTraceEventParser.Keywords.All ) 
+    traceEventSession.EnableKernelProvider(KernelTraceEventParser.Keywords.All, KernelTraceEventParser.Keywords.None) |> ignore
+    traceEventSession.EnableProvider(ClrTraceEventParser.ProviderGuid, TraceEventLevel.Verbose, keywords) |> ignore
+    traceEventSession
