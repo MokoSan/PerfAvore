42e0b00faa9e345b625ced9f9643873bad66f522
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2021-12-18 03:06:50 -0800

    Added rouge programs and json rule files associated with them.

---
+++ src/Console/RougePrograms/ThreadPoolStarvations/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using System.Threading;
+
+while (true)
+{
+    Task.Run(() => {
+        System.Console.WriteLine("Before Wait.");
+        Task.Delay(2000).Wait();
+        System.Console.WriteLine("Before After.");
+    });
+
+    // Rate of creating task < Rate of waits.
+    Task.Delay(500);
+}
---
+++ src/Console/RougePrograms/TimedExcessiveAllocs/Program.cs
@@ -0,0 +1,12 @@
+﻿while(true)
+{
+    // LOH Alloc.
+    byte[] alloc1 = new byte[1024 * 1024 * 1024];
+    System.Console.WriteLine($"Allocating a byte array of size: {1024 * 1024 * 1024}");
+
+    // SOH Alloc.
+    byte[] alloc2 = new byte[1024 * 80];
+    System.Console.WriteLine($"Allocating a byte array of size: {1024 * 80}");
+
+    System.Threading.Thread.Sleep(500);
+}
