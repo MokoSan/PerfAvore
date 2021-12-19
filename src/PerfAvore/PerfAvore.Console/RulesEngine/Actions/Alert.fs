8a6bfbd7485a421da9c1a0f5df8336928b05db6a
Mukund Raghav Sharma <mukund.sharma92@gmail.com>
2021-12-19 03:54:03 -0800

    Added all the code to the notebook

--- src/PerfAvore/PerfAvore.Console/RulesEngine/Actions/Alert.fs
+++ src/PerfAvore/PerfAvore.Console/RulesEngine/Actions/Alert.fs
@@ -12,11 +12,11 @@ let printAlert (rule : Rule) (traceEvent : TraceEvent) : unit =
     table.Title <- TableTitle "[underline red] Alert! [/]"
     table.Title.Style <- Style()
 
-    table.AddColumn("Input Rule") |> ignore
-    table.AddColumn("Timestamp") |> ignore
-    table.AddColumn("Event Name") |> ignore
-    table.AddColumn("Event Property") |> ignore
-    table.AddColumn("Payload") |> ignore
+    table.AddColumn("Input Rule")      |> ignore
+    table.AddColumn("Timestamp")       |> ignore
+    table.AddColumn("Event Name")      |> ignore
+    table.AddColumn("Event Property")  |> ignore
+    table.AddColumn("Payload")         |> ignore
 
     table.AddRow( rule.InputRule, 
                   traceEvent.TimeStampRelativeMSec.ToString(), 
